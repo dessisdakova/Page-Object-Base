a80e7902c87d985fcd8c5726c965b0dc762488bd
Desislava Dakova <dessisdakova@Desislavas-MacBook-Pro.local>
2025-02-26 13:27:32 +0200

    change in fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,16 +15,15 @@ def browser():
     options.add_argument("--disable-notifications")
 
     # local runs
-    # driver = webdriver.Chrome(options=options, service=Service(ChromeDriverManager().install()))
+    driver = webdriver.Chrome(options=options, service=Service(ChromeDriverManager().install()))
 
     # container runs
     """
     chrome_binary_path = "/opt/google/chrome/chrome"
     options.binary_location = chrome_binary_path
-    """
     service = Service(executable_path="/usr/local/bin/chromedriver")
     driver = webdriver.Chrome(service=service, options=options)
-
+    """
 
     # using image for browser
     # driver = webdriver.Remote("http://browser:4444", options=options)
