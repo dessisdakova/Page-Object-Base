3d3803a703bf65785818aefb76f94f3b5ae78659
Desislava Dakova <dessisdakova@Desislavas-MacBook-Pro.local>
2025-02-26 13:07:15 +0200

    added docker and compose file

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,11 +13,20 @@ def browser():
     options.add_argument("--headless")
     options.add_argument("--disable-logging")
     options.add_argument("--disable-notifications")
-    # driver = webdriver.Chrome(options=options, service=Service(ChromeDriverManager().install()))
-    # driver = webdriver.Chrome(options=options)
 
+    # local runs
+    # driver = webdriver.Chrome(options=options, service=Service(ChromeDriverManager().install()))
+
+    # container runs
+    """
     chrome_binary_path = "/opt/google/chrome/chrome"
     options.binary_location = chrome_binary_path
-    driver = webdriver.Chrome(options=options)
+    """
+    service = Service(executable_path="/usr/local/bin/chromedriver")
+    driver = webdriver.Chrome(service=service, options=options)
+
+
+    # using image for browser
+    # driver = webdriver.Remote("http://browser:4444", options=options)
     yield driver
     driver.quit()
